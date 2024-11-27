a55625c97bae9d3df68a736f01a237e737cc46a2
CaiZhixin <caizhixin@buaa.edu.cn>
2024-11-27 15:25:13 +0800

    更新CSRF信任源，增加新域名和端口

--- Ugo_Online/Ugo_Online/settings.py
+++ Ugo_Online/Ugo_Online/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "django-insecure-7#c1kv3)6b^+k*g-y!98(7q0p9u26dlvt0p_85zl%804vhdf32
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://8.152.218.70:8000',]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://8.152.218.70:8000', 'http://8.152.218.70', 'http://8.152.218.70:3000']
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '[::1]']
 
 # coookie
