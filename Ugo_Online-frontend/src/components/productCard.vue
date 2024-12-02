df75b8cab5958c535d0224bc5fd07b6a401cd9f7
钟昊翔 <21231025@buaa.edu.cn>
2024-12-02 19:54:43 +0800

    修改商品样式

--- Ugo_Online-frontend/src/components/productCard.vue
+++ Ugo_Online-frontend/src/components/productCard.vue
@@ -1,32 +1,34 @@
 <template>
 	<v-card height="100%">
-		<v-img :src="product.image" aspect-ratio="1" />
-		<v-divider></v-divider>
-		<v-card-item class="px-2 pb-0">
-			<span style="font-size: 16px; color: red; font-weight: bold">￥</span>
-			<span class="font-weight-bold text-h5" style="color: red;">
-				{{ product.price }}
-			</span>
-		</v-card-item>
-		<v-card-title class="py-0">
-			{{ product.name }}
-			<v-chip size="x-small" color="primary" class="mb-1">{{ product.category_name }}</v-chip>
-		</v-card-title>
-		<v-card-subtitle class="py-0">库存：{{ product.stock_quantity }}</v-card-subtitle>
-		<v-card-text class="pt-2 pb-0">{{ product.description }}</v-card-text>
-		<v-card-actions>
-			<v-btn
-				v-if="user.role === 'CUSTOMER'"
-				color="primary"
-				@click="onclickAdd2Card"
-				prepend-icon="mdi-cart"
-			>加入购物车</v-btn>
-			<v-btn
-				v-if="user.role === 'SELLER'"
-				color="primary"
-				@click="showUpdateProduct = true"
-			>编辑商品</v-btn>
-		</v-card-actions>
+		<div class="d-flex flex-column justify-space-between" style="height: 100%">
+			<v-img :src="product.image" aspect-ratio="1" />
+			<v-divider></v-divider>
+			<v-card-item class="px-2 pb-0">
+				<span style="font-size: 16px; color: red; font-weight: bold">￥</span>
+				<span class="font-weight-bold text-h5" style="color: red;">
+					{{ product.price }}
+				</span>
+			</v-card-item>
+			<v-card-title class="py-0">
+				{{ product.name }}
+				<v-chip size="x-small" color="primary" class="mb-1">{{ product.category_name }}</v-chip>
+			</v-card-title>
+			<v-card-subtitle class="py-0">库存：{{ product.stock_quantity }}</v-card-subtitle>
+			<v-card-text class="pt-2 pb-0">{{ product.description }}</v-card-text>
+			<v-card-actions>
+				<v-btn
+					v-if="user.role === 'CUSTOMER'"
+					color="primary"
+					@click="onclickAdd2Card"
+					prepend-icon="mdi-cart"
+				>加入购物车</v-btn>
+				<v-btn
+					v-if="user.role === 'SELLER'"
+					color="primary"
+					@click="showUpdateProduct = true"
+				>编辑商品</v-btn>
+			</v-card-actions>
+		</div>
 	</v-card>
 	
 	<v-dialog v-model="showUpdateProduct" transition="dialog-bottom-transition">
