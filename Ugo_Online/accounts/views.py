cace4b3548884725a3a03ab3b43b15bfaa9fa685
CaiZhixin <caizhixin@buaa.edu.cn>
2024-12-03 02:32:54 +0800

    debug

--- Ugo_Online/accounts/views.py
+++ Ugo_Online/accounts/views.py
@@ -191,7 +191,7 @@ class AddMoneyView(APIView):
         serializer = AddMoneySerializer(instance=request.user, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            new_message(request.user, "您为您的账户充值了" + str(serializer.validated_data['money']) + "元，账户当前余额为 " + str(request.user.money) + " 元。")
+            new_message(request.user, "充值成功，账户当前余额为 " + str(request.user.money) + " 元。")
             return api_response(True, message='充值成功', data=serializer.data)
         else:
             return api_response(False, code=500, message=get_error_message(serializer.errors), data=serializer.errors)
