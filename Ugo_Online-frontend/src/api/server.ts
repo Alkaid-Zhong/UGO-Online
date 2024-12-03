976f9df61c263b137f9d555e7f5ab55d2585d707
钟昊翔 <21231025@buaa.edu.cn>
2024-12-04 00:55:11 +0800

    remove useless import

--- Ugo_Online-frontend/src/api/server.ts
+++ Ugo_Online-frontend/src/api/server.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import snackbar from "./snackbar";
 import router from "@/router";
-import { head } from "node_modules/axios/index.cjs";
 
 interface Response {
 	success: boolean;
