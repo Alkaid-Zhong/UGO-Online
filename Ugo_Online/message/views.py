212eed64074bcc86fc1eb8cfa480442ad81f3b70
CaiZhixin <caizhixin@buaa.edu.cn>
2024-12-05 15:40:44 +0800

    Add message listing with pagination and response formatting

--- Ugo_Online/message/views.py
+++ Ugo_Online/message/views.py
@@ -5,7 +5,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-from Ugo_Online.utils import api_response
+from Ugo_Online.utils import api_response, list_response
 from accounts.permissions import IsCustomer
 from message.models import Message
 from message.serializers import MessageSerializer
@@ -20,6 +20,17 @@ class MessageListView(ListView):
 
     def get_queryset(self):
         return Message.objects.filter(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            paginated_response = self.get_paginated_response(serializer.data).data
+            return list_response(paginated_response, self.paginator, 'messages')
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            return api_response(True, code=0, data={'messages': serializer.data})
 
 
 class MessageView(APIView):
