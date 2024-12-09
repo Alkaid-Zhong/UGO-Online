11e0481ca900e6796db8959708260390ae1a9e7d
CaiZhixin <caizhixin@buaa.edu.cn>
2024-12-09 16:46:58 +0800

    更新用户字符串显示格式

--- Ugo_Online/accounts/models.py
+++ Ugo_Online/accounts/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def __str__(self):
-        return f'[{self.email}] {self.name}'
+        return f'{self.email}({self.name})'
 
 
 class Address(models.Model):
