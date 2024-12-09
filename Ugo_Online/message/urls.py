35c5972b208d8f13add9e01b9a16e34a35ec0da3
CaiZhixin <caizhixin@buaa.edu.cn>
2024-12-09 22:26:42 +0800

    修复URL配置中的name参数缺失

--- Ugo_Online/message/urls.py
+++ Ugo_Online/message/urls.py
@@ -5,5 +5,5 @@ from message.views import MessageListView, MessageView, ReadAllMessageView
 urlpatterns = [
     path('', MessageListView.as_view(), name='message_list'),
     path('<int:message_id>/', MessageView.as_view(), name='message_detail'),
-    path('read_all/', ReadAllMessageView.as_view(), 'read_all'),
+    path('read_all/', ReadAllMessageView.as_view(), name='read_all'),
 ]
